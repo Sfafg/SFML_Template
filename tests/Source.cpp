e504aa93c579f0fe676ef2486abb8dbc15de669f
Sławek <slawekzdunek2.3.6@gmail.com>
2026-03-24 19:24:38 +0100

    Added green circle

--- tests/Source.cpp
+++ tests/Source.cpp
@@ -1,14 +1,17 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({400, 400}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
-        while (auto event = window.pollEvent()) {
+        while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) window.close();
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
 }
